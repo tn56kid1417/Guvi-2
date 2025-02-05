685ac1be323fc431936d34f03fb844aca44d10e3
tn56kid1417 <140372692+tn56kid1417@users.noreply.github.com>
2025-02-05 10:15:32 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,6 @@
     sh 'chmod +x build.sh'
     sh './build.sh'
     docker login -u TN56KID -p 9791583365
-    docker tag test TN56KID/ar
-    docker push TN56KID/ar
+    docker tag test TN56KID/pipeline
+    docker push TN56KID/pipeline
     
